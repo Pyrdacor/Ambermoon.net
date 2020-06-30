3b86aa7a5800e97fc43ce8aa7a61a55e4a993fe5
Pyrdacor <trobt@web.de>
2020-06-30 17:45:28 +0200

    Improved palette graphic reading

--- Ambermoon.Data.Legacy/GraphicReader.cs
+++ Ambermoon.Data.Legacy/GraphicReader.cs
@@ -4,6 +4,44 @@ namespace Ambermoon.Data.Legacy
 {
     public class GraphicReader : IGraphicReader
     {
+        void ReadPaletteGraphic(Graphic graphic, IDataReader dataReader, int planes, Palette palette)
+        {
+            if (palette == null)
+                throw new ArgumentNullException("Legacy palette graphics need a palette.");
+
+            var data = dataReader.ReadBytes(graphic.Width * graphic.Height * planes / 8); // as width is always a multiple of 8 this will work
+            int planeSize = graphic.Width / 8; // as width is always a multiple of 8 this will work
+            int bitIndex = 0;
+            int byteIndex = 0;
+            int offset = 0;
+
+            for (int y = 0; y < graphic.Height; ++y)
+            {
+                for (int x = 0; x < graphic.Width; ++x)
+                {
+                    int paletteIndex = 0;
+
+                    for (int p = 0; p < planes; ++p)
+                    {
+                        if ((data[offset + p * planeSize + byteIndex] & (1 << (7 - bitIndex))) != 0)
+                            paletteIndex |= (1 << p);
+                    }
+
+                    palette.Fill(graphic.Data, (x + y * graphic.Width) * 4, paletteIndex);
+
+                    if (++bitIndex == 8)
+                    {
+                        bitIndex = 0;
+                        ++byteIndex;
+                    }
+                }
+
+                offset += planes * planeSize;
+                byteIndex = 0;
+                bitIndex = 0;
+            }
+        }
+
         public void ReadGraphic(Graphic graphic, IDataReader dataReader, GraphicInfo? graphicInfo)
         {
             // Legacy graphics need the graphicInfo.
@@ -17,77 +55,13 @@ namespace Ambermoon.Data.Legacy
             switch (graphicInfo.Value.GraphicFormat)
             {
                 case GraphicFormat.Palette5Bit:
-                    {
-                        var palette = graphicInfo.Value.Palette;
-
-                        if (palette == null)
-                            throw new ArgumentNullException("Legacy palette graphics need a palette.");
-
-                        var data = dataReader.ReadBytes(width * height * 5 / 8); // as width is always a multiple of 8 this will work
-                        int planeSize = width / 8; // as width is always a multiple of 8 this will work
-                        int bitIndex = 0;
-                        int byteIndex = 0;
-                        int offset = 0;
-
-                        for (int y = 0; y < height; ++y)
-                        {
-                            for (int x = 0; x < width; ++x)
-                            {
-                                int paletteIndex = 0;
-
-                                for (int p = 0; p < 5; ++p)
-                                {
-                                    if ((data[offset + p * planeSize + byteIndex] & (1 << (7 - bitIndex))) != 0)
-                                        paletteIndex |= (1 << p);
-                                }
-
-                                palette.Fill(graphic.Data, (x + y * width) * 4, paletteIndex);
-
-                                if (++bitIndex == 8)
-                                {
-                                    bitIndex = 0;
-                                    ++byteIndex;
-                                }
-                            }
-
-                            offset += 5 * planeSize;
-                            byteIndex = 0;
-                            bitIndex = 0;
-                        }
-                    }
+                    ReadPaletteGraphic(graphic, dataReader, 5, graphicInfo?.Palette);
                     break;
-                case GraphicFormat.Texture4Bit:
-                    {
-                        byte[] palette = new byte[]
-                        {
-                            0, 0, 0, 255,
-                            0, 0, 170, 255,
-                            170, 0, 0, 255,
-                            170, 0, 170, 255,
-                            0, 170, 0, 255,
-                            0, 170, 170, 0, 255,
-                            170, 170, 0, 255,
-                            170, 170, 170, 255,
-                            85, 85, 85, 255,
-                            0, 0, 255, 255,
-                            255, 0, 0, 255,
-                            255, 0, 255, 255,
-                            0, 255, 0, 255,
-                            0, 255, 255, 255,
-                            255, 255, 0, 255,
-                            255, 255, 255, 255
-                        };
-                        var bitReader = new BitReader(dataReader.ReadBytes((width * height * 4 + 7) / 8));
-
-                        for (int i = 0; i < width * height; ++i)
-                        {
-                            int index = bitReader.ReadBits(4);
-                            graphic.Data[i * 4 + 0] = palette[index * 4 + 0];
-                            graphic.Data[i * 4 + 1] = palette[index * 4 + 1];
-                            graphic.Data[i * 4 + 2] = palette[index * 4 + 2];
-                            graphic.Data[i * 4 + 3] = 255;
-                        }
-                    }
+                case GraphicFormat.Palette4Bit:
+                    ReadPaletteGraphic(graphic, dataReader, 4, graphicInfo?.Palette);
+                    break;
+                case GraphicFormat.Palette3Bit:
+                    ReadPaletteGraphic(graphic, dataReader, 3, graphicInfo?.Palette);
                     break;
                 case GraphicFormat.XRGB16:
                     for (int i = 0; i < width * height; ++i)
