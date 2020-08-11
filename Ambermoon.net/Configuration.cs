12f78f999cf18ef543de80e5514deca331c1084d
Pyrdacor <trobt@web.de>
2020-08-11 14:07:19 +0200

    Fixed data path for published application

--- Ambermoon.net/Configuration.cs
+++ Ambermoon.net/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
@@ -9,7 +10,55 @@ namespace Ambermoon
         public int Width { get; set; } = 1280;
         public int Height { get; set; } = 800;
         public bool Fullscreen { get; set; } = false;
-        public string DataPath { get; set; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public string DataPath { get; set; } = ExecutablePath;
+
+        public static string ExecutablePath
+        {
+            get
+            {
+                const bool isWindows = true; // TODO
+
+                var assemblyPath = Process.GetCurrentProcess().MainModule.FileName;
+
+                if (assemblyPath.EndsWith("dotnet"))
+                {
+                    assemblyPath = Assembly.GetExecutingAssembly().Location;
+                }
+
+                var assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+
+                if (isWindows)
+                {
+                    if (assemblyDirectory.EndsWith(@"Debug") || assemblyDirectory.EndsWith(@"Release"))
+                    {
+                        string projectFile = Path.GetFileNameWithoutExtension(assemblyPath) + ".csproj";
+
+                        var root = new DirectoryInfo(assemblyDirectory);
+
+                        while (root.Parent != null)
+                        {
+                            if (File.Exists(Path.Combine(root.FullName, projectFile)))
+                                break;
+
+                            root = root.Parent;
+
+                            if (root.Parent == null) // we could not find it (should not happen)
+                                return assemblyDirectory;
+                        }
+
+                        return root.FullName;
+                    }
+                    else
+                    {
+                        return assemblyDirectory;
+                    }
+                }
+                else
+                {
+                    return assemblyDirectory;
+                }
+            }
+        }
 
         public static Configuration Load(string filename)
         {
