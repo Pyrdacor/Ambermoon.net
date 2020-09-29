9cb76b418f8f8774cc9dbded072fbe909577c9f1
Pyrdacor <trobt@web.de>
2020-09-29 12:11:24 +0200

    Added missing file

---
+++ Ambermoon.Data.Common/Enumerations/Option.cs
@@ -0,0 +1,11 @@
+﻿namespace Ambermoon.Data.Enumerations
+{
+    public enum Option
+    {
+        Music = 0x01,
+        FastBattleMode = 0x02,
+        TextJustification = 0x04,
+        FloorTexture3D = 0x08,
+        CeilingTexture3D = 0x10
+    }
+}
