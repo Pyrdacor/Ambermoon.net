db1feba762524257ca21080d948c273c5c4d3cc0
Pyrdacor <trobt@web.de>
2020-11-01 08:01:07 +0100

    Added automap class

---
+++ Ambermoon.Data.Common/Automap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ambermoon.Data
+{
+    public class Automap
+    {
+        public byte[] ExplorationBits { get; set; }
+
+        public bool IsBlockExplored(Map map, uint x, uint y)
+        {
+            return IsBlockExplored(x + y * (uint)map.Width);
+        }
+
+        public bool IsBlockExplored(uint blockIndex)
+        {
+            int byteIndex = (int)blockIndex / 8;
+
+            if (byteIndex >= ExplorationBits.Length)
+                throw new AmbermoonException(ExceptionScope.Data, "Block index exceeds automap size.");
+
+            int bitIndex = (int)blockIndex % 8;
+
+            return (ExplorationBits[byteIndex] & (1 << bitIndex)) != 0;
+        }
+
+        public void ExploreBlock(Map map, uint x, uint y)
+        {
+            ExploreBlock(x + y * (uint)map.Width);
+        }
+
+        public void ExploreBlock(uint blockIndex)
+        {
+            int byteIndex = (int)blockIndex / 8;
+
+            if (byteIndex >= ExplorationBits.Length)
+                throw new AmbermoonException(ExceptionScope.Data, "Block index exceeds automap size.");
+
+            int bitIndex = (int)blockIndex % 8;
+
+            ExplorationBits[byteIndex] |= (byte)(1 << bitIndex);
+        }
+
+        public void ResetExploration()
+        {
+            Array.Fill(ExplorationBits, (byte)0);
+        }
+    }
+}
