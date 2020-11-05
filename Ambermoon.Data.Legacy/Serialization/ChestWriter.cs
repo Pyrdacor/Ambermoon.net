4c703c24b0c7228ebebb5bc8b95d2b0666ff143a
Pyrdacor <trobt@web.de>
2020-11-05 12:35:24 +0100

    Added chest and merchant writer

---
+++ Ambermoon.Data.Legacy/Serialization/ChestWriter.cs
@@ -0,0 +1,21 @@
+﻿using Ambermoon.Data.Serialization;
+
+namespace Ambermoon.Data.Legacy.Serialization
+{
+    public class ChestWriter : IChestWriter
+    {
+        public void WriteChest(Chest chest, IDataWriter dataWriter)
+        {
+            for (int y = 0; y < 4; ++y)
+            {
+                for (int x = 0; x < 6; ++x)
+                {
+                    ItemSlotWriter.WriteItemSlot(chest.Slots[x, y], dataWriter);
+                }
+            }
+
+            dataWriter.Write((ushort)chest.Gold);
+            dataWriter.Write((ushort)chest.Food);
+        }
+    }
+}
---
+++ Ambermoon.Data.Legacy/Serialization/MerchantWriter.cs
@@ -0,0 +1,18 @@
+﻿using Ambermoon.Data.Serialization;
+
+namespace Ambermoon.Data.Legacy.Serialization
+{
+    public class MerchantWriter : IMerchantWriter
+    {
+        public void WriteMerchant(Merchant merchant, IDataWriter dataWriter)
+        {
+            for (int y = 0; y < 4; ++y)
+            {
+                for (int x = 0; x < 6; ++x)
+                {
+                    ItemSlotWriter.WriteItemSlot(merchant.Slots[x, y], dataWriter);
+                }
+            }
+        }
+    }
+}
