6d3d3e4ba935c0e9146148515b91e134b983a7a5
Pyrdacor <trobt@web.de>
2020-12-09 14:59:17 +0100

    Fixed paths in pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
 
 if ($isWindows) {
   Write-Host Pack zip for Windows
-  7z a Ambermoon.net-Windows.zip "%APPVEYOR_BUILD_FOLDER%\Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"
+  7z a Ambermoon.net-Windows.zip "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"
 } else {
   Write-Host Pack tar.gz for Linux
-  7z a Ambermoon.net-Linux.tar "%APPVEYOR_BUILD_FOLDER%\Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"
+  7z a Ambermoon.net-Linux.tar "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net.exe"
   7z a Ambermoon.net-Linux.tar.gz Ambermoon.net-Linux.tar
   rm Ambermoon.net-Linux.tar
 }
