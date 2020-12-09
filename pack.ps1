0e07f1bd55e6ada1457da8b2bb5235e7bac8ddfc
Pyrdacor <trobt@web.de>
2020-12-09 15:23:42 +0100

    fixed wrong linux assembly name

--- pack.ps1
+++ pack.ps1
@@ -5,7 +5,7 @@ if ($isWindows) {
   7z a Ambermoon.net-Windows.zip "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"
 } else {
   Write-Host Pack tar.gz for Linux
-  7z a Ambermoon.net-Linux.tar "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net.exe"
+  7z a Ambermoon.net-Linux.tar "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net"
   7z a Ambermoon.net-Linux.tar.gz Ambermoon.net-Linux.tar
   rm Ambermoon.net-Linux.tar
 }
