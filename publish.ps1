75b8ea35c19e290877c1eb9e1c93c364d5e180ec
Pyrdacor <trobt@web.de>
2021-01-27 12:01:17 +0100

    Fixed publish script

--- publish.ps1
+++ publish.ps1
@@ -5,15 +5,15 @@ if ($isWindows) {
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore
   Write-Host Pack zips for Windows
-  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"+"Ambermoon.net\versions.dat" "Ambermoon.net\Ambermoon.net.exe"
+  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows.zip "Ambermoon.net\Ambermoon.net.exe"
-  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x86\publish\Ambermoon.net.exe"+"Ambermoon.net\versions.dat" "Ambermoon.net\Ambermoon.net.exe"
+  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x86\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows32Bit.zip "Ambermoon.net\Ambermoon.net.exe"
 } else {
   Write-Host Publish Linux executable
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore
   Write-Host Pack tar for Linux
-  cat "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net" "./Ambermoon.net/versions.dat" > "./Ambermoon.net/Ambermoon.net"
+  cat "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net" "./versions.dat" > "./Ambermoon.net/Ambermoon.net"
   7z a Ambermoon.net-Linux.tar "./Ambermoon.net/Ambermoon.net"
   7z a Ambermoon.net-Linux.tar.gz Ambermoon.net-Linux.tar
   rm Ambermoon.net-Linux.tar
