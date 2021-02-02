6d3ffb4c17ef43a44ec7adc7e11e55d57035bf7d
Pyrdacor <trobt@web.de>
2021-02-02 12:47:31 +0100

    Added IntroFont resource

---
+++ Ambermoon.net/Resources.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Resources;
+
+namespace Ambermoon
+{
+    internal class Resources
+    {        
+        static System.Resources.ResourceManager resourceManager;
+        
+        [SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources()
+        {
+        }
+        
+        [EditorBrowsableAttribute(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceManager, null))
+                    resourceManager = new ResourceManager("Ambermoon.Resources", typeof(Resources).Assembly);
+                return resourceManager;
+            }
+        }
+        
+        [EditorBrowsableAttribute(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get;
+            set;
+        }
+        
+        internal static byte[] IntroFont => (byte[])ResourceManager.GetObject("IntroFont", Culture);
+    }
+}
