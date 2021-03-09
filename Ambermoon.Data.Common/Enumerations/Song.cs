53cf874c87be3f7bdb8adb49ed4261bbce5b9095
Pyrdacor <trobt@web.de>
2021-03-09 09:08:42 +0100

    Added song enumeration

---
+++ Ambermoon.Data.Common/Enumerations/Song.cs
@@ -0,0 +1,135 @@
+﻿namespace Ambermoon.Data.Enumerations
+{
+    // Note: Menu and Intro songs are not part of this.
+    public enum Song
+    {
+        /// <summary>
+        /// Gnome mine
+        /// </summary>
+        WhoSaidHiHo,
+        /// <summary>
+        /// Morag world map
+        /// </summary>
+        MellowCamelFunk,
+        /// <summary>
+        /// Dwarfs, forest moon indoor
+        /// </summary>
+        CloseToTheHedge,
+        /// <summary>
+        /// Gemstone
+        /// </summary>
+        VoiceOfTheBagpipe,
+        /// <summary>
+        /// Spannenberg
+        /// </summary>
+        Downtown,
+        /// <summary>
+        /// Ship
+        /// </summary>
+        Ship,
+        /// <summary>
+        /// Eagle
+        /// </summary>
+        WholeLottaDove,
+        /// <summary>
+        /// Horse
+        /// </summary>
+        HorseIsNoDisgrace,
+        /// <summary>
+        /// Tavern I
+        /// </summary>
+        DontLookBach,
+        /// <summary>
+        /// Tavern II
+        /// </summary>
+        RoughWaterfrontTavern,
+        /// <summary>
+        /// Battle theme
+        /// </summary>
+        SapphireFireballsOfPureLove,
+        /// <summary>
+        /// Temple of life
+        /// </summary>
+        TheAumRemainsTheSame,
+        /// <summary>
+        /// Capital
+        /// </summary>
+        Capital,
+        /// <summary>
+        /// Lyramion world map
+        /// </summary>
+        PloddingAlong,
+        /// <summary>
+        /// Magical fly disc
+        /// </summary>
+        CompactDisc,
+        /// <summary>
+        /// Melancholic music
+        /// </summary>
+        RiversideTravellingBlues,
+        /// <summary>
+        /// Thief guild
+        /// </summary>
+        NobodysVaultButMine,
+        /// <summary>
+        /// Grandfather's cellar
+        /// </summary>
+        LaCryptaStrangiato,
+        /// <summary>
+        /// Alchemist tower
+        /// </summary>
+        MistyDungeonHop,
+        /// <summary>
+        /// Witch's broom
+        /// </summary>
+        BurnBabyBurn,
+        /// <summary>
+        /// Camp, Inn
+        /// </summary>
+        BarBrawlin,
+        /// <summary>
+        /// Morag temples?
+        /// </summary>
+        PsychedelicDuneGroove,
+        /// <summary>
+        /// Level up
+        /// </summary>
+        StairwayToLevel50,
+        /// <summary>
+        /// Undeads
+        /// </summary>
+        ThatHunchIsBack,
+        /// <summary>
+        /// Nera's house?
+        /// </summary>
+        ChickenSoup,
+        /// <summary>
+        /// Luminor's tower
+        /// </summary>
+        DragonChaseInCreepyDungeon,
+        /// <summary>
+        /// Harp, Matthias in Illien
+        /// </summary>
+        HisMastersVoice,
+        /// <summary>
+        /// Empty song?
+        /// </summary>
+        NoName,
+        /// <summary>
+        /// Magic effect (touching pulsing lights, wishing well, etc)
+        /// </summary>
+        OhNoNotAnotherMagicalEvent,
+        /// <summary>
+        /// Dramatic events
+        /// </summary>
+        TheUhOhSong,
+        /// <summary>
+        /// Grandfather theme
+        /// </summary>
+        OwnerOfALonelySword,
+        /// <summary>
+        /// Game over
+        /// </summary>
+        GameOver
+    }
+}
