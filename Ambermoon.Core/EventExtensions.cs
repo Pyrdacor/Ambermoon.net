3e3ca6c7893c050fb5c891cdfd19f89f5f33d110
Pyrdacor <trobt@web.de>
2021-04-27 11:39:05 +0200

    Fixed issue with storing tile changes in savegames

--- Ambermoon.Core/EventExtensions.cs
+++ Ambermoon.Core/EventExtensions.cs
@@ -236,6 +236,14 @@ namespace Ambermoon
 
                     // Add it to the savegame as well.
                     // Note: Savegame stores the front tile index for 2D and wall/object index for 3D.
+                    // Note: If map index is 0 (same map) we have to replace it with the real map index
+                    // for savegames. Otherwise it will be interpreted as "end of tile changes marker".
+                    if (changeTileEvent.MapIndex == 0)
+                        changeTileEvent.MapIndex = map.Index;
+                    if (changeTileEvent.X == 0)
+                        changeTileEvent.X = x + 1;
+                    if (changeTileEvent.Y == 0)
+                        changeTileEvent.Y = y + 1;
                     game.CurrentSavegame.TileChangeEvents.SafeAdd(map.Index, changeTileEvent);
                     break;
                 }
