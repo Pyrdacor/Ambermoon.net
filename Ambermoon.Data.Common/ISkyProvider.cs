69fde5828622f8a32c6d6c7b92dfa1fde4181a4b
Pyrdacor <trobt@web.de>
2021-05-01 02:39:12 +0200

    Added missing files

---
+++ Ambermoon.Data.Common/ISkyProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Ambermoon.Data
+{
+    public class SkyPart
+    {
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public uint Color { get; set; }
+    }
+
+    public interface ISkyProvider
+    {
+        IEnumerable<SkyPart> GetSkyParts(Map map, uint hour, uint minute);
+    }
+}
