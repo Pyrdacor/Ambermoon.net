7a7199e1e7d9046d8fcb0e71c35f43996ce65352
Pyrdacor <trobt@web.de>
2021-07-28 13:45:01 +0200

    This is 0.9.5 RC2

--- Ambermoon.net/GameWindow.cs
+++ Ambermoon.net/GameWindow.cs
@@ -919,7 +919,7 @@ namespace Ambermoon
             var videoMode = new VideoMode(60);
             var options = new WindowOptions(true, new WindowDimension(100, 100),
                 new WindowDimension(Width, Height), 60.0, 60.0, GraphicsAPI.Default,
-                $"Ambermoon.net v{version.Major}.{version.Minor}.{version.Build} - Test Release",
+                $"Ambermoon.net v{version.Major}.{version.Minor}.{version.Build} RC2",
                 WindowState.Normal, WindowBorder.Fixed, true, false, videoMode, 24);
 
             try
