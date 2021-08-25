b11113f7071638e81b09751853496b19df10a99d
Pyrdacor <trobt@web.de>
2021-08-25 15:20:46 +0200

    Added tileset writer

---
+++ Ambermoon.Data.Legacy/Serialization/TilesetWriter.cs
@@ -0,0 +1,20 @@
+﻿using Ambermoon.Data.Serialization;
+
+namespace Ambermoon.Data.Legacy.Serialization
+{
+    public class TilesetWriter
+    {
+        public void WriteTileset(Tileset tileset, IDataWriter dataWriter)
+        {
+            dataWriter.Write((ushort)tileset.Tiles.Length);
+
+            foreach (var tile in tileset.Tiles)
+            {
+                dataWriter.Write((uint)tile.Flags);
+                dataWriter.Write((ushort)tile.GraphicIndex);
+                dataWriter.Write((byte)tile.NumAnimationFrames);
+                dataWriter.Write(tile.ColorIndex);
+            }
+        }
+    }
+}
