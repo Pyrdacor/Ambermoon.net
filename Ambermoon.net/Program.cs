7214b646158ee8ec4d3c04a49e0e63fdb03c4ac1
Pyrdacor <trobt@web.de>
2021-09-30 19:08:01 +0200

    Added manual temp file cleanup

--- Ambermoon.net/Program.cs
+++ Ambermoon.net/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Ambermoon
 {
@@ -60,6 +62,7 @@ namespace Ambermoon
             finally
             {
                 SaveConfig(configuration);
+                DotnetCleanup();
             }
         }
 
@@ -82,6 +85,81 @@ namespace Ambermoon
             }
 
             Console.WriteLine(message + Environment.NewLine + ex.StackTrace);
+        }
+
+        static void DotnetCleanup()
+        {
+            // As of netcore 3.1 the self-contained assembly will extract
+            // all dependencies to a temp location. This is not cleaned up
+            // automatically. So we try to do so on termination.
+            // TODO: Maybe later (.NET6 etc) we can remove this.
+            bool isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            string mainExecutable = Assembly.GetEntryAssembly().Location;
+            string appFolder = Path.TrimEndingDirectorySeparator(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+            if (isWindows)
+            {
+                CleanUpTempFolder(Path.TrimEndingDirectorySeparator(Path.GetTempPath()));
+            }
+            else
+            {
+                CleanUpTempFolder("/var/tmp");
+                CleanUpTempFolder("~");
+            }
+
+            void TryDeleteFile(string path)
+            {
+                try
+                {
+                    File.Delete(path);
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+
+            void TryDeleteDirectory(string path, bool onlyClearContent = false)
+            {
+                try
+                {
+                    var files = Directory.GetFiles(path);
+
+                    foreach (var file in files)
+                        TryDeleteFile(file);
+
+                    var subDirectories = Directory.GetDirectories(path);
+
+                    foreach (var subDirectory in subDirectories)
+                        TryDeleteDirectory(subDirectory);
+
+                    if (!onlyClearContent)
+                        Directory.Delete(path);
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+
+            void CleanUpTempFolder(string tempRootFolder)
+            {
+                string escapedTempDirectory = Path.DirectorySeparatorChar == '/' ? tempRootFolder : tempRootFolder.Replace("\\", "\\\\");
+                string guidPattern = @"[^/\\]+";
+                string escapedDirSeparator = Path.DirectorySeparatorChar == '\\' ? "\\\\" : Path.DirectorySeparatorChar.ToString();
+                var tempRegex = new Regex($"^{escapedTempDirectory}{escapedDirSeparator}.net{escapedDirSeparator}Ambermoon.net{escapedDirSeparator}{guidPattern}$");
+
+                if (tempRegex.IsMatch(appFolder))
+                {
+                    TryDeleteDirectory(appFolder, true);
+
+                    foreach (var directory in Directory.GetDirectories(Path.GetDirectoryName(appFolder)))
+                    {
+                        if (appFolder != directory && tempRegex.IsMatch(directory))
+                            TryDeleteDirectory(directory);
+                    }
+                }
+            }
         }
     }
 }
