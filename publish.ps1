953e4a1c3c90488123a61f45bd899aaf947a2204
Pyrdacor <trobt@web.de>
2021-12-10 19:33:46 +0100

    Adjusted publish script for NET6

--- publish.ps1
+++ publish.ps1
@@ -5,10 +5,12 @@ if ($isWindows) {
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r win-x86 --no-restore --nologo --self-contained
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r win-x64 --no-restore --nologo --self-contained
   Write-Host Pack standalone zips for Windows
-  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x64\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
+  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\net6\win-x64\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows.zip "Ambermoon.net\Ambermoon.net.exe"
-  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\netcoreapp3.1\win-x86\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
+  7z a Ambermoon.net-Windows.zip "Ambermoon.net\bin\Any CPU\Release\net6\win-x64\publish\api-ms-win-core-winrt-l1-1-0.dll"
+  cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\net6\win-x86\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows32Bit.zip "Ambermoon.net\Ambermoon.net.exe"
+  7z a Ambermoon.net-Windows32Bit.zip "Ambermoon.net\bin\Any CPU\Release\net6\win-x86\publish\api-ms-win-core-winrt-l1-1-0.dll"
 } elseif ($isLinux) {
   Write-Host Publish Linux executable
   Set-Variable -Name UseGLES -Value false
@@ -16,8 +18,8 @@ if ($isWindows) {
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r linux-x64 --no-restore --self-contained
   dotnet publish -c Release "./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj" -r linux-x64 --no-restore
   Write-Host Pack tar for Linux
-  Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net"'
-  7z a Ambermoon.net-Linux.tar "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net"
+  Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/net6/linux-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.net"'
+  7z a Ambermoon.net-Linux.tar "./Ambermoon.net/bin/Any CPU/Release/net6/linux-x64/publish/Ambermoon.net"
   7z a Ambermoon.net-Linux.tar.gz Ambermoon.net-Linux.tar
   rm Ambermoon.net-Linux.tar
 } else {
@@ -25,10 +27,10 @@ if ($isWindows) {
   dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -r osx-x64 --no-restore --self-contained
   dotnet publish -c Release "./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj" -r osx-x64 --no-restore
   Write-Host Pack zips for Mac
-  Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.net"'
-  7z a Ambermoon.net-Mac.zip "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.net"
+  Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/net6/osx-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.net"'
+  7z a Ambermoon.net-Mac.zip "./Ambermoon.net/bin/Any CPU/Release/net6/osx-x64/publish/Ambermoon.net"
   mkdir -p ./bundle/Ambermoon.net/Ambermoon.net.app/Contents/MacOS/
   cp -r ./Ambermoon.net/Mac/* ./bundle/Ambermoon.net/
-  cp "./Ambermoon.net/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.net" ./bundle/Ambermoon.net/Ambermoon.net.app/Contents/MacOS/
+  cp "./Ambermoon.net/bin/Any CPU/Release/net6/osx-x64/publish/Ambermoon.net" ./bundle/Ambermoon.net/Ambermoon.net.app/Contents/MacOS/
   7z a Ambermoon.net-Mac-Bundle.zip ./bundle/Ambermoon.net/
 }
