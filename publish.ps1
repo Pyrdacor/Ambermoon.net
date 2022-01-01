695057f4330abd41783cc295f61177f75b831b14
Pyrdacor <trobt@web.de>
2022-01-01 16:47:57 +0100

    Fixed builds

--- publish.ps1
+++ publish.ps1
@@ -2,20 +2,20 @@ $ErrorActionPreference = 'Stop';
 
 if ($isWindows) {
   Write-Host Publish Windows executables
-  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -r win-x86 --no-restore --nologo --self-contained
-  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -r win-x64 --no-restore --nologo --self-contained
+  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r win-x86 --no-restore --nologo --self-contained
+  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r win-x64 --no-restore --nologo --self-contained
   Write-Host Pack standalone zips for Windows
   cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x64\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows.zip "Ambermoon.net\Ambermoon.net.exe"
-  7z a Ambermoon.net-Windows7.zip "Ambermoon.net\Ambermoon.net.exe" "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x64\publish\api-ms-win-core-winrt-l1-1-0.dll"
+  7z a Ambermoon.net-Windows7.zip "Ambermoon.net\Ambermoon.net.exe" "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x64\publish\x64\api-ms-win-core-winrt-l1-1-0.dll"
   cmd /c copy /b "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x86\publish\Ambermoon.net.exe"+"versions.dat" "Ambermoon.net\Ambermoon.net.exe"
   7z a Ambermoon.net-Windows32Bit.zip "Ambermoon.net\Ambermoon.net.exe"
-  7z a Ambermoon.net-Windows7-32Bit.zip "Ambermoon.net\Ambermoon.net.exe" "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x86\publish\api-ms-win-core-winrt-l1-1-0.dll"
+  7z a Ambermoon.net-Windows7-32Bit.zip "Ambermoon.net\Ambermoon.net.exe" "Ambermoon.net\bin\Any CPU\Release\net6.0\win-x86\publish\x86\api-ms-win-core-winrt-l1-1-0.dll"
 } elseif ($isLinux) {
   Write-Host Publish Linux executable
   Set-Variable -Name UseGLES -Value false
   dotnet build -c Release "./Ambermoon.Renderer.OpenGL/Ambermoon.Renderer.OpenGL.csproj"
-  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -r linux-x64 --no-restore --self-contained
+  dotnet publish -c Release "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-x64 --no-restore --self-contained
   dotnet publish -c Release "./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj" -r linux-x64 --no-restore
   Write-Host Pack tar for Linux
   Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/netcoreapp3.1/linux-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/Release/net6.0/linux-x64/publish/Ambermoon.net"'
@@ -24,7 +24,7 @@ if ($isWindows) {
   rm Ambermoon.net-Linux.tar
 } else {
   Write-Host Publish Mac executable
-  dotnet publish -c ReleaseMac "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -r osx-x64 --no-restore --self-contained
+  dotnet publish -c ReleaseMac "./Ambermoon.net/Ambermoon.net.csproj" -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r osx-x64 --no-restore --self-contained
   dotnet publish -c Release "./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj" -r osx-x64 --no-restore
   Write-Host Pack zips for Mac
   Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Any CPU/Release/netcoreapp3.1/osx-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList '"./versions.dat"','"./Ambermoon.net/bin/Any CPU/ReleaseMac/netcoreapp3.1/osx-x64/publish/Ambermoon.net"'
