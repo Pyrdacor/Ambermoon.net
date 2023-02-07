c9d765963b0d65bae2b8d6427266d6625ce4d4db
Dimitris Panokostas <midwan@gmail.com>
2023-02-07 19:59:01 +0100

    bugfix: build-arm64.sh contained reference to linux-x64

--- build-arm64.sh
+++ build-arm64.sh
@@ -1,9 +1,9 @@
 mkdir publish-arm64
 copy versions.dat publish-arm64
-dotnet publish -c Debug ./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-x64 --nologo --self-contained -o ./publish-arm64
+dotnet publish -c Debug ./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-arm64 --nologo --self-contained -o ./publish-arm64
 dotnet publish -c Debug ./Ambermoon.net/Ambermoon.net.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-arm64 --nologo --self-contained -o ./publish-arm64
 dotnet publish -c Debug ./AmbermoonPatcher/AmbermoonPatcher.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-arm64 --nologo --self-contained -o ./publish-arm64
-cd publish-v
-Ambermoon.ConcatFiles versions versions.dat patcher AmbermoonPatcher Ambermoon.net
+cd publish-arm64
+./Ambermoon.ConcatFiles versions versions.dat patcher AmbermoonPatcher Ambermoon.net
 rm ./versions.dat
 rm ./Ambermoon.ConcatFiles
