ab53ddac0c5e20d6bd757b6c9626eb0c96eb8a98
Dimitris Panokostas <midwan@gmail.com>
2023-02-13 17:08:49 +0100

    Updated powershell scripts with new config name

--- publish-linux-arm64.ps1
+++ publish-linux-arm64.ps1
@@ -1,12 +1,12 @@
 Write-Host Publish Linux-arm64 executable
 Set-Variable -Name UseGLES -Value true
 dotnet clean "./Ambermoon.Renderer.OpenGL/Ambermoon.Renderer.OpenGL.csproj"
-dotnet build -c ReleaseAndroid "./Ambermoon.Renderer.OpenGL/Ambermoon.Renderer.OpenGL.csproj"
-dotnet publish -c ReleaseAndroid ./Ambermoon.net/Ambermoon.net.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-arm64 --no-restore --self-contained
-dotnet publish -c ReleaseAndroid ./AmbermoonPatcher/AmbermoonPatcher.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-arm64 --nologo --self-contained
+dotnet build -c ReleaseES "./Ambermoon.Renderer.OpenGL/Ambermoon.Renderer.OpenGL.csproj"
+dotnet publish -c ReleaseES ./Ambermoon.net/Ambermoon.net.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-arm64 --no-restore --self-contained
+dotnet publish -c ReleaseES ./AmbermoonPatcher/AmbermoonPatcher.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-arm64 --nologo --self-contained
 Write-Host Pack tar for Linux
-Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Release/net7.0/win-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList 'versions','"./versions.dat"','diffs','diffs.dat','"./Ambermoon.net/bin/ReleaseAndroid/net7.0/linux-arm64/publish/Ambermoon.net"'
+Start-Process -FilePath "./Ambermoon.ConcatFiles/bin/Release/net7.0/win-x64/publish/Ambermoon.ConcatFiles" -Wait -WorkingDirectory . -ArgumentList 'versions','"./versions.dat"','diffs','diffs.dat','"./Ambermoon.net/bin/ReleaseES/net7.0/linux-arm64/publish/Ambermoon.net"'
 
-7z a Ambermoon.net-Linux-arm64.tar "./Ambermoon.net/bin/ReleaseAndroid/net7.0/linux-arm64/publish/Ambermoon.net" "./AmbermoonPatcher/bin/ReleaseAndroid/net7.0/linux-arm64/publish/AmbermoonPatcher" "./Package/*"
+7z a Ambermoon.net-Linux-arm64.tar "./Ambermoon.net/bin/ReleaseES/net7.0/linux-arm64/publish/Ambermoon.net" "./AmbermoonPatcher/bin/ReleaseES/net7.0/linux-arm64/publish/AmbermoonPatcher" "./Package/*"
 7z a Ambermoon.net-Linux-arm64.tar.gz Ambermoon.net-Linux-arm64.tar -mx9
 rm Ambermoon.net-Linux-arm64.tar
