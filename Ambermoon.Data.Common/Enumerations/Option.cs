6b8af6f792549df670c33a27e0b5964230eabf61
Pyrdacor <trobt@web.de>
2024-09-15 19:28:55 +0200

    Add new potential outro option

--- Ambermoon.Data.Common/Enumerations/Option.cs
+++ Ambermoon.Data.Common/Enumerations/Option.cs
@@ -7,6 +7,8 @@
         TextJustification = 0x04,
         FloorTexture3D = 0x08,
         CeilingTexture3D = 0x10,
+        // TODO: For later possible adjusted ending (Advanced only)
+        ValdynTalkedToSheera = 0x4000,
         FoundYellowSphere = 0x8000
     }
 }
