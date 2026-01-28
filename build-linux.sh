ad92abdc4c225d6e5655fb43fe855e82832e140a
Pyrdacor <trobt@web.de>
2026-01-28 15:22:54 +0100

    Fixed local build scripts for win and linux

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,14 @@
-mkdir publish-linux
+#!/bin/sh
+
+mkdir -p publish-linux
 cp ./Ambermoon.net/versions.dat publish-linux
 cp ./Ambermoon.net/diffs.dat publish-linux
 dotnet publish -c Debug ./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-x64 --nologo --self-contained -o ./publish-linux
 dotnet publish -c Debug ./Ambermoon.net/Ambermoon.net.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-x64 --nologo --self-contained -o ./publish-linux
 dotnet publish -c Debug ./AmbermoonPatcher/AmbermoonPatcher.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r linux-x64 --nologo --self-contained -o ./publish-linux
-cd publish-v
-Ambermoon.ConcatFiles versions versions.dat patcher AmbermoonPatcher Ambermoon.net
+cd publish-linux
+./Ambermoon.ConcatFiles versions versions.dat diffs diffs.dat patcher AmbermoonPatcher Ambermoon.net
 rm ./versions.dat
 rm ./diffs.dat
 rm ./Ambermoon.ConcatFiles
+rm ./AmbermoonPatcher.exe
--- build-win64.cmd
+++ build-win64.cmd
@@ -1,4 +1,4 @@
-mkdir publish-win64
+mkdir publish-win64 2>nul
 copy Ambermoon.net\versions.dat publish-win64\versions.dat
 copy Ambermoon.net\diffs.dat publish-win64\diffs.dat
 dotnet publish -c Debug ./Ambermoon.ConcatFiles/Ambermoon.ConcatFiles.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r win-x64 --nologo --self-contained -o ./publish-win64
@@ -6,6 +6,7 @@ dotnet publish -c Debug ./Ambermoon.net/Ambermoon.net.csproj -p:PublishSingleFil
 dotnet publish -c Debug ./AmbermoonPatcher/AmbermoonPatcher.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true  -r win-x64 --nologo --self-contained -o ./publish-win64
 cd publish-win64
 Ambermoon.ConcatFiles.exe versions versions.dat diffs diffs.dat patcher AmbermoonPatcher.exe Ambermoon.net.exe
-rm ./versions.dat
-rm ./diffs.dat
-rm ./Ambermoon.ConcatFiles.exe
+del versions.dat
+del diffs.dat
+del Ambermoon.ConcatFiles.exe
+del AmbermoonPatcher.exe
